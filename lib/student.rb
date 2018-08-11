f78b75a8a069a78e465f080e7fca0d5efd6dd6b7
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-08-11 23:38:15 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -42,5 +42,11 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end
 
+  def update
+    sql = "UPDATE students SET name = ?, grade = ? WHERE id = ?"
+    
+    DB[:conn].execute(sql, self.name, self.album, self.id)    
+  end
+
 
 end
